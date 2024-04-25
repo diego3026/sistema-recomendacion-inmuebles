3bcaa6e091cb2c2670fd56d3e0e20dfb711244ba
diego <diegoonate3026@gmail.com>
2024-04-25 17:00:24 -0500

    staticfiles quitando

--- sistema_inmuebles/settings.py
+++ sistema_inmuebles/settings.py
@@ -170,9 +170,9 @@ AUTH_USER_MODEL = 'inmobiliaria.Usuario'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-   os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+# )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
