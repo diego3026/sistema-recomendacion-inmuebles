03c980523139aaa5d74625f64b9ab58b024e2b08
Diego Oñate <90926333+diego3026@users.noreply.github.com>
2024-04-29 17:53:22 -0500

    Update settings.py

--- sistema_inmuebles/settings.py
+++ sistema_inmuebles/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'rest_framework_swagger',
     'rest_framework_simplejwt',
     'drf_yasg',
+    'corsheaders,
     #app
     'inmobiliaria',
 ]
@@ -64,6 +65,7 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -116,7 +118,9 @@ DATABASES = {
 }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173/",
+]
 
 LOGGING = {
     'version': 1,
