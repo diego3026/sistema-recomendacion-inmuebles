5e1b4d251ccd52b8f61551421a70e8a2a59a567b
diego <diegoonate3026@gmail.com>
2024-04-29 20:17:50 -0500

    actualizando rama

--- sistema_inmuebles/settings.py
+++ sistema_inmuebles/settings.py
@@ -52,8 +52,8 @@ INSTALLED_APPS = [
 ]
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=15),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
