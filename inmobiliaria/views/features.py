03e9fe6e94e2eb488af7caf59cdb03e0734da6d6
Diego Oñate <90926333+diego3026@users.noreply.github.com>
2024-05-11 14:38:04 -0500

    Update features.py

--- inmobiliaria/views/features.py
+++ inmobiliaria/views/features.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from inmobiliaria.permissions import IsSuperUser
@@ -10,14 +11,19 @@ class TipoDeCaracteristicaViewSet(viewsets.ModelViewSet):
     queryset = TipoDeCaracteristica.objects.all()
     serializer_class = TipoDeCaracteristicaSerializer
 
+    permission_classes = [IsAuthenticated]
+    def get(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
     @action(detail=True, methods=['delete'],permission_classes=[IsSuperUser])
     def delete(self, request, *args, **kwargs):
         instance = self.get_object()
         self.perform_destroy(instance)
         return Response({'status': 'tipo de característica deleted'}, status=status.HTTP_204_NO_CONTENT)
     
-    permission_classes = [IsSuperUser]
+
     def update(self, request, *args, **kwargs):
+        permission_classes = [IsSuperUser]
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -41,6 +47,10 @@ class CaracteristicaViewSet(viewsets.ModelViewSet):
     queryset = Caracteristica.objects.all()
     serializer_class = CaracteristicaSerializer
 
+    permission_classes = [IsAuthenticated]
+    def get(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
     # Agregar acción delete
     @action(detail=True, methods=['delete'],permission_classes=[IsSuperUser])
     def delete(self, request, *args, **kwargs):
@@ -48,8 +58,9 @@ class CaracteristicaViewSet(viewsets.ModelViewSet):
         self.perform_destroy(instance)
         return Response({'status': 'caracteristica deleted'}, status=status.HTTP_204_NO_CONTENT)
 
-    permission_classes = [IsSuperUser]
+
     def update(self, request, *args, **kwargs):
+        permission_classes = [IsSuperUser]
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data)
         serializer.is_valid(raise_exception=True)
