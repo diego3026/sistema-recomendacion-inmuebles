c6945a16252b4557fa1eb9d6324fc9b704b28f09
Diego Oñate <90926333+diego3026@users.noreply.github.com>
2024-05-21 11:47:22 -0500

    Update settings.py

--- sistema_inmuebles/settings.py
+++ sistema_inmuebles/settings.py
@@ -122,7 +122,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
 #    "http://localhost:5173",
 #    "http://localhost:8080",
 #]
-CORS_ORIGIN_ALLOW_ALL = true 
+CORS_ORIGIN_ALLOW_ALL = True 
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
